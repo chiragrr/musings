577b8ba1ea0c1384cea381d7adc1ceda780d2527
Chirag Rajyaguru <chiragrr@gmail.com>
2017-06-26 21:56:24 -0700

    Added measurements

--- go_sort_measurement.go
+++ go_sort_measurement.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+  "math/rand"
+  "time"
 )
 
 func reset_a()[]int{
-  a := []int {12, 23, 55, 17, 211, 13}
+  var a [5000]int
+  for i:=0; i < 5000; i++ {
+    a[i] =  rand.Intn(65000)
+  }
   return (a[:])
 }
 
 func main() {
     a := reset_a()
-    fmt.Println(a)
-
+    pre_bub := time.Now().UnixNano()
     bubblesort(a[:])
+    post_bub := time.Now().UnixNano()
+
     a = reset_a()
+    pre_merge := time.Now().UnixNano()
     mergesort(a[:])
+    post_merge := time.Now().UnixNano()
+
+    fmt.Println("Time taken(nano sec) while sorting 5K Integers using bubble sort :", post_bub - pre_bub)
+    fmt.Println("Time taken(nano sec) while sorting 5K Integers using merge sort :", post_merge - pre_merge)
 }
 
 func bubblesort(a []int){
@@ -29,21 +40,23 @@ func bubblesort(a []int){
       }
     }
   }
-  fmt.Println(a)
+  //fmt.Println(a)
 }
+
+
 func mergesort(a []int){
   first_half := len(a)/2
   j := first_half
   second_half := len(a)
   var copy_array = make ([] int, len(a))
   var i, count = 0, 0
-  fmt.Println(a[0:first_half], a[first_half:second_half])
+  //fmt.Println(a[0:first_half], a[first_half:second_half])
   for i < first_half && j < second_half {
     if a[i] < a[j]{
       copy_array[count] = a[i]
       count = count + 1
       i = i + 1
-    } else {
+    } else{
       copy_array[count] = a[j]
       j = j + 1
       count = count + 1
@@ -60,5 +73,5 @@ func mergesort(a []int){
     count = count + 1
   }
 
-  fmt.Println(copy_array)
+  //fmt.Println(copy_array)
 }
